127f21de3dd4312713105a03fe5d0cd08f2e702b
joy langat <joy.chemutai@student.moringaschool.com>
2022-02-16 12:38:02 +0300

    add imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
-import { DateCountPipe } from './date-count.pipe';
-import { DatePipePipe } from './date-pipe.pipe';
 import { DatepipePipe } from './datepipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfilePageComponent,
-    DateCountPipe,
-    DatePipePipe,
     DatepipePipe
   ],
   imports: [
