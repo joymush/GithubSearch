a24338d8900f6e505cb82fea2e6900a1bfdc99d8
joy langat <joy.chemutai@student.moringaschool.com>
2022-02-16 12:44:24 +0300

    add classes to handle user details

--- src/app/user-info.ts
+++ src/app/user-info.ts
@@ -1,2 +1,4 @@
 export class UserInfo {
+    constructor (public login: string, public followers: number, public following: number, public bio:string, public created_at: Date, public avatar_url:string,){
+    }
 }
